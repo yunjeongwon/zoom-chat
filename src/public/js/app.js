2781752ca89b9e5a4b9e96152a71c269919667bf
윤정원 <wjd10577@naver.com>
2022-09-12 19:27:30 +0900

    create websocket server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,1 @@
-hello;
+alert("hi");
--- src/server.js
+++ src/server.js
@@ -6,9 +6,13 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.render("home");
 });
+
+app.get("/*", (_, res) => {
+  res.redirect("/");
+})
 
 app.listen(3000, () => {
   console.log(`Listening on http://localhost:3000`);
